d5af6488b1e46db82231352533f2b462133a223f
Florin Mirica <miricaflorin@hotmail.com>
2019-04-05 16:23:20 -0700

    continue working on methods to allow adding and editing car specs

--- src/app/garage/component.ts
+++ src/app/garage/component.ts
@@ -10,4 +10,23 @@ import {
 import { DatePipe } from "@angular/common";
 
 import { CarObject } from "../car-object.model";
-import { GarageApi } from "../garage/garage-api.service";
+
+@Component({
+	selector: "app-garage",
+	templateUrl: "./garage.component.html",
+	styleUrls: ["./garage.component.css"]
+})
+export class GarageComponent implements OnInit {
+	@Input() cars: CarObject[];
+	@Output() updateEvent = new EventEmitter();
+
+	allowEdit: boolean[] = [];
+
+	constructor(private garageApi: GarageApi) {}
+
+	ngOnInit() {
+		this.allowEdit = new Array(this.cars.length);
+		for (let i: number = 0; i < this.cars.length; i++) {
+			this.allowEdit[i] = false;
+		}
+	}
